c3ef4fae7faf7272f0eb95308a0b03f78d87afbe
Stevie <stevierinkler369@googlemail.com>
2026-06-30 23:07:51 +0200

    Notes added

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ import psutil
 first_network = psutil.net_io_counters()
 second_network= psutil.net_io_counters(pernic= False, nowrap= True)
 cpu_value = psutil.cpu_percent(interval=1)
+STYLE = {"bg": "#1e1e1e", "fg": "#00ff99", "font": ("Courier", 12, "bold")}
 
-#cpu_warning_shown = False
 label_cpu_warning = None
 network_window = None
 cpu_window = None
@@ -16,6 +16,8 @@ label_cpu = None
 label_ram = None
 label_total_ram = None
 label_mem_percentage = None
+
+# CPU function
 
 def show_cpu():
     global cpu_window
@@ -32,7 +34,7 @@ def show_cpu():
         cpu_window = tk.Toplevel(root)
         cpu_window.title("CPU")
         cpu_window.geometry("400x120")
-        label_cpu = tk.Label(cpu_window, bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_cpu = tk.Label(cpu_window,**STYLE)
         label_cpu.place(x=100, y=25)
         cpu_window.configure(bg="#1e1e1e")
         label_cpu_warning = tk.Label(cpu_window, bg="#1e1e1e")
@@ -50,9 +52,7 @@ def show_cpu():
     label_cpu.config(text= f"CPU Usage: {cpu_value} %",bg="#1e1e1e", font=("Courier", 12, "bold"))
     cpu_window.after(3000, show_cpu)
         
-    #if cpu_value >= 1 and not cpu_warning_shown:
-    #    messagebox.showwarning("WARNING", "CPU Usage too high!")
-    #    cpu_warning_shown = True
+# Ram function
 
 def show_ram():
     global ram_window
@@ -76,21 +76,21 @@ def show_ram():
         ram_window.geometry("400x150")
         ram_window.configure(bg="#1e1e1e")
                 
-        label_total_ram = tk.Label(ram_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_total_ram = tk.Label(ram_window, **STYLE)
         label_total_ram.place(x=50, y=20)
 
-        label_ram = tk.Label(ram_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_ram = tk.Label(ram_window, **STYLE)
         label_ram.place(x=50, y=50)
 
-        label_mem_percentage = tk.Label(ram_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_mem_percentage = tk.Label(ram_window, **STYLE)
         label_mem_percentage.place (x=50, y=80)
 
         label_ram_warning = tk.Label(ram_window, bg="#1e1e1e")
         label_ram_warning.place(x=50, y=110)
 
-    label_total_ram.config(text= f"Total memory: {total_memory:.2f} GB",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_ram.config(text= f"Total available memory: {available_mem:.2f} GB",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_mem_percentage.config(text= f"Using: {mem.percent:.2f} % of RAM",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+    label_total_ram.config(text= f"Total memory: {total_memory:.2f} GB", **STYLE)
+    label_ram.config(text= f"Total available memory: {available_mem:.2f} GB",**STYLE)
+    label_mem_percentage.config(text= f"Using: {mem.percent:.2f} % of RAM",**STYLE)
 
     if mem_perc >= 80:
         label_ram_warning.config(text="Warning: RAM Usage too high!", fg="#EF0307", font=("Courier", 12, "bold"))
@@ -103,6 +103,7 @@ def show_ram():
 
     ram_window.after(3000, show_ram)
 
+# Network function
 def show_network():
     global network_window
     global first_network
@@ -132,25 +133,27 @@ def show_network():
         network_window.geometry("500x200")
         network_window.configure(bg="#1e1e1e")
 
-        label_total_send = tk.Label(network_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_total_send = tk.Label(network_window,**STYLE)
         label_total_send.place(x=50, y=30)
 
-        label_total_received = tk.Label(network_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_total_received = tk.Label(network_window,**STYLE)
         label_total_received.place(x=50, y= 60)
 
-        label_interval_send = tk.Label(network_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_interval_send = tk.Label(network_window,**STYLE)
         label_interval_send.place(x=50, y=150)
 
-        label_interval_received = tk.Label(network_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_interval_received = tk.Label(network_window,**STYLE)
         label_interval_received.place(x=50, y=120)
     
-    label_total_send.config(text= f"Number of total bytes sent : {total_bytes_send:.2f} Mb",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_total_received.config(text= f"Number of total bytes received : {total_bytes_recv:.2f} Mb",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_interval_received.config(text= f"Number of bytes sent : {interval_bytes_send:.2f} Mb",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_interval_send.config(text= f"Number of bytes received : {interval_bytes_recv:.2f} Mb",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+    label_total_send.config(text= f"Number of total bytes sent : {total_bytes_send:.2f} Mb",**STYLE)
+    label_total_received.config(text= f"Number of total bytes received : {total_bytes_recv:.2f} Mb",**STYLE)
+    label_interval_received.config(text= f"Number of bytes sent : {interval_bytes_send:.2f} Mb",**STYLE)
+    label_interval_send.config(text= f"Number of bytes received : {interval_bytes_recv:.2f} Mb",**STYLE)
 
-    network_window.after(3000, show_network)
-        
+    network_window.after(15000, show_network)
+
+# Storage/Disk function 
+
 def show_storage():
     global storage_window
     global label_total_storage
@@ -167,11 +170,11 @@ def show_storage():
         storage_window.geometry("400x200")
         storage_window.configure(bg="#1e1e1e")
 
-        label_total_storage = tk.Label(storage_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_total_storage = tk.Label(storage_window,**STYLE)
         label_total_storage.place (x=50, y=30)
-        label_used_storage = tk.Label(storage_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_used_storage = tk.Label(storage_window,**STYLE)
         label_used_storage.place (x=50, y= 70)
-        label_percentage_storage_used = tk.Label(storage_window,bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+        label_percentage_storage_used = tk.Label(storage_window,**STYLE)
         label_percentage_storage_used.place (x=50, y= 110)
 
     disk = psutil.disk_usage('/')
@@ -179,9 +182,9 @@ def show_storage():
     used_storage = (disk.used / 1024**3)
     storage_used_percentage = (disk.percent)
 
-    label_total_storage.config(text= f"total existing storage: {total_storage:.0f} GB",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_used_storage.config(text= f"used storage: {used_storage:.0f} GB",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
-    label_percentage_storage_used.config(text= f"{storage_used_percentage} % of storage is used",bg="#1e1e1e", fg="#00ff99", font=("Courier", 12, "bold"))
+    label_total_storage.config(text= f"total existing storage: {total_storage:.0f} GB",**STYLE)
+    label_used_storage.config(text= f"used storage: {used_storage:.0f} GB",**STYLE)
+    label_percentage_storage_used.config(text= f"{storage_used_percentage} % of storage is used",**STYLE)
 
     storage_window.after(3000, show_storage)
 
